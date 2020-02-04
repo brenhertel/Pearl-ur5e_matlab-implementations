6dcc9f173f5cb1db0d02ac80a26e2ad4007a2f89
brenhertel <brendan_hertel@student.uml.edu>
2020-02-04 08:57:02 -0800

    2/4/2020

--- lte_grid.m
+++ lte_grid.m
@@ -1,16 +1,14 @@
 %%% lte grid deformations %%%
+clear all;
 filename = 'hello2.h5';
 
 x = h5read(filename, '/hello/resampled_x');
 y = h5read(filename, '/hello/resampled_y');
 
 traj = [x; y]';
-figure;
-plot(x, y);
-hold on;
 len = length(x);
 init_end_d = calc_euclidean(x(1), y(1), x(len), y(len));
-disp(init_end_d);
+%disp(init_end_d);
 
 grid_size = 10;
 grid_increment = init_end_d / grid_size;
@@ -23,15 +21,42 @@ end
 
 for i = 1:grid_size
     for j = 1:grid_size
-        plot(grid_x(i), grid_y(j), 'k+');
-        hold on;
+        %plot(grid_x(i), grid_y(j), 'k+');
+        %hold on;
         lte_fixed_points = [1       ([grid_x(i) grid_y(j)]);
-                            len     ([x(len)    y(len)   ])];
-        [lte_deformed_x, lte_deformed_y] = lte(traj, lte_fixed_points);
-        plot(lte_deformed_x, lte_deformed_y);
-        pause; 
-        hold on;
+            len     ([x(len)    y(len)   ])];
+        [lte_deformed_x{i, j}, lte_deformed_y{i, j}] = lte(traj, lte_fixed_points);
     end
+end
+figure;
+% hold on;
+% for i = 1:grid_size
+%     for j = 1:grid_size
+%         plot(grid_x(i), grid_y(j), 'k+');
+%     end
+% end
+% plot(x, y, 'r');
+
+for k = 1:i * j
+    a = floor(k / i) + 1;
+    b = mod(k, j);
+    if (b == 0)
+        b = 10;
+    end
+    a = constrain(a, 1, 10);
+    b = constrain(b, 1, 10);
+    subplot(grid_size, grid_size, k);
+    plot(lte_deformed_x{b, 11 - a}, lte_deformed_y{b, 11 - a});
+end
+
+function out = constrain(in, lower, upper)
+if (in < lower)
+    out = lower;
+elseif (in > upper)
+    out = upper;
+else
+    out = in;
+end
 end
 
 function max_d = calc_max_d_window(x1, y1, x2, y2, first, last)
