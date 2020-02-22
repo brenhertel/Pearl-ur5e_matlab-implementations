e2757cc0c3f792c4b47a9d1b81a17aa6e9c0a31c
brenhertel <brendan_hertel@student.uml.edu>
2020-02-21 21:22:33 -0500

    2/21/2020

--- draw_to_h5.m
+++ draw_to_h5.m
@@ -1,13 +1,17 @@
 %%% store drawing in .h5 file %%%
 %% preprocess trajs %%
-x_traj = xx;
-y_traj = yy;
+x_traj = xx * 100;
+y_traj = yy * 100;
 num_points = 1000;
 x_traj = preprocess_traj(x_traj, num_points);
 y_traj = preprocess_traj(y_traj, num_points);
 %% store in file %%
 name = 'Circle';
-h5create([name '_drawing_demo.h5'],['/' name '/x'],[1 num_points])
-h5write([name '_drawing_demo.h5'],['/' name '/x'], x_traj)
-h5create([name '_drawing_demo.h5'],['/' name '/y'],[1 num_points])
-h5write([name '_drawing_demo.h5'],['/' name '/y'], y_traj)
+filename = [name '_drawing_demo.h5'];
+h5create(filename,['/' name '/x'],[1 num_points])
+h5write(filename,['/' name '/x'], x_traj)
+h5create(filename,['/' name '/y'],[1 num_points])
+h5write(filename,['/' name '/y'], y_traj)
+
+% move the file to proper folder
+movefile(filename, 'h5 files/');
